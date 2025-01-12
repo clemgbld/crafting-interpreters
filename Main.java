2622634d65bfd1a2ea4ee65315ee0938ad73008d
cgombauld <clement.gombauld@cpexterne.org>
2025-01-12 15:55:27 +0100

    completed first part of the challenge

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    /*
+   Get a “Hello, world!” program written and running in Java. Set up whatever makefiles or IDE projects you need to get it working. If you have a debugger, get comfortable with it and step through your program as it runs.
+     */
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+
+}
