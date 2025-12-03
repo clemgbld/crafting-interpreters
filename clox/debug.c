648b686049b441545c343d8fe2a411227f128ba9
cgombauld <clement.gombauld@cpexterne.org>
2025-12-03 21:13:38 +0100

    feat(chapter-21): implement global variables support

--- clox/debug.c
+++ clox/debug.c
@@ -46,6 +46,14 @@ int dissassembleInstruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_TRUE", offset);
   case OP_FALSE:
     return simpleInstruction("OP_FALSE", offset);
+  case OP_POP:
+    return simpleInstruction("OP_POP", offset);
+  case OP_GET_GLOBAL:
+    return constantInstruction("OP_GET_GLOBAL", chunk, offset);
+  case OP_DEFINE_GLOBAL:
+    return constantInstruction("OP_DEFINE_GlOBAL", chunk, offset);
+  case OP_SET_GLOBAL:
+    return constantInstruction("OP_SET_GlOBAL", chunk, offset);
   case OP_EQUAL:
     return simpleInstruction("OP_EQUAL", offset);
   case OP_LESS:
@@ -64,6 +72,8 @@ int dissassembleInstruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_NOT", offset);
   case OP_NEGATE:
     return simpleInstruction("OP_NEGATE", offset);
+  case OP_PRINT:
+    return simpleInstruction("OP_PRINT", offset);
   default:
     printf("Unknown opcode %d\n", instruction);
     return offset + 1;
