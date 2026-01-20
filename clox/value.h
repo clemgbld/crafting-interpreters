7792fbbf0b09da0aabb1a015262ee438ccefaa3f
cgombauld <clement.gombauld@cpexterne.org>
2026-01-20 10:04:39 +0100

    feat: finished book

--- clox/value.h
+++ clox/value.h
@@ -1,3 +1,4 @@
+//> Chunks of Bytecode value-h
 #ifndef clox_value_h
 #define clox_value_h
 
@@ -8,37 +9,31 @@
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
 
-#ifndef NAN_BOXING
+#ifdef NAN_BOXING
 
 #define SIGN_BIT ((uint64_t)0x8000000000000000)
 #define QNAN ((uint64_t)0x7ffc000000000000)
 
-#define TAG_NIL 1   // 01
-#define TAG_FALSE 2 // 10
-#define TAG_TRUE 3  // 11
+#define TAG_NIL 1   // 01.
+#define TAG_FALSE 2 // 10.
+#define TAG_TRUE 3  // 11.
 
 typedef uint64_t Value;
 
-#define IS_NIL(value) ((value) == TAG_NIL)
-
 #define IS_BOOL(value) (((value) | 1) == TRUE_VAL)
-
+#define IS_NIL(value) ((value) == NIL_VAL)
 #define IS_NUMBER(value) (((value) & QNAN) != QNAN)
-
-#define AS_NUMBER(value) valueToNum(value)
+#define IS_OBJ(value) (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
 
 #define AS_BOOL(value) ((value) == TRUE_VAL)
-
-#define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
-
-#define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
-
-#define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define AS_NUMBER(value) valueToNum(value)
+#define AS_OBJ(value) ((Obj *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
 
 #define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
-
-#define NUMBER_VAL(value) numToValue(value)
-
+#define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
 #define OBJ_VAL(obj) (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
 
 static inline double valueToNum(Value value) {
@@ -53,44 +48,75 @@ static inline Value numToValue(double num) {
   return value;
 }
 
-#endif
+#else
 
-typedef enum { VAL_BOOL, VAL_NIL, VAL_NUMBER, VAL_OBJ } ValueType;
+//< Optimization nan-boxing
+//> Types of Values value-type
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL, // [user-types]
+  VAL_NUMBER,
+  //> Strings val-obj
+  VAL_OBJ
+  //< Strings val-obj
+} ValueType;
 
+//< Types of Values value-type
+/* Chunks of Bytecode value-h < Types of Values value
+typedef double Value;
+*/
+//> Types of Values value
 typedef struct {
   ValueType type;
   union {
     bool boolean;
     double number;
+    //> Strings union-object
     Obj *obj;
-  } as;
+    //< Strings union-object
+  } as; // [as]
 } Value;
+//< Types of Values value
+//> Types of Values is-macros
 
 #define IS_BOOL(value) ((value).type == VAL_BOOL)
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+//> Strings is-obj
 #define IS_OBJ(value) ((value).type == VAL_OBJ)
+//< Strings is-obj
+//< Types of Values is-macros
+//> Types of Values as-macros
 
+//> Strings as-obj
+#define AS_OBJ(value) ((value).as.obj)
+//< Strings as-obj
 #define AS_BOOL(value) ((value).as.boolean)
 #define AS_NUMBER(value) ((value).as.number)
-#define AS_OBJ(value) ((value).as.obj)
+//< Types of Values as-macros
+//> Types of Values value-macros
 
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+//> Strings obj-val
 #define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+//< Strings obj-val
+//< Types of Values value-macros
+//> Optimization end-if-nan-boxing
+
+#endif
 
 typedef struct {
   int capacity;
   int count;
   Value *values;
-  Value *stackTop;
 } ValueArray;
 
 bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray *array);
-void freeValueArray(ValueArray *array);
 void writeValueArray(ValueArray *array, Value value);
+void freeValueArray(ValueArray *array);
 void printValue(Value value);
 
 #endif
